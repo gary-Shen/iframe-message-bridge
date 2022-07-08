e39be8aea69b0137553320fcc754235896349e76
沈关林 <grinson50@gmail.com>
2022-07-08 17:00:19 +0800

    initial commit

---
+++ src/EventEmitter.ts
@@ -0,0 +1,37 @@
+import EventEmitter3, { EventNames } from 'eventemitter3';
+
+export default class EventEmitter extends EventEmitter3 {
+  constructor() {
+    super();
+  }
+
+  public has(name: EventNames<string>): boolean {
+    return this.eventNames().includes(name);
+  }
+
+  // @ts-ignore
+  public emit(event: EventNames<string>, ...args: any[]): Promise<any> {
+    const prefix = EventEmitter3.prefixed as string;
+    const evt = prefix ? prefix + event : event;
+    // @ts-ignore
+    const events = this._events;
+
+    if (!events[evt]) return Promise.reject();
+
+    const listeners = events[evt];
+
+    if (listeners.fn) {
+      if (listeners.once) this.removeListener(event, listeners.fn, undefined, true);
+
+      return Promise.resolve(listeners.fn.apply(listeners.context, args));
+    } else {
+      const results = [];
+      for (let i = 0; i < listeners.length; i++) {
+        if (listeners[i].once) this.removeListener(event, listeners[i].fn, undefined, true);
+        results[i] = Promise.resolve(listeners[i].fn.apply(listeners[i].context, args));
+      }
+
+      return Promise.all(results);
+    }
+  }
+}
